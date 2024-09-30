a78a313a7b7a4965f66b7eeff82e11f764c2d493
Kawser-45 <kawserramadan754@gmail.com>
2024-09-30 18:52:46 +0600

    test

---
+++ custom.js
@@ -0,0 +1,320 @@
+
+
+
+  
+//  Task-1:
+
+for (let i = 1; i <= 200; i++) {
+  console.log(i);
+  if (i === 100) {
+    console.log("Loop is terminated at 100");
+    break;
+  }
+}
+
+
+//  Task-2
+
+let sum = 0;
+for (let i = 1; i <= 100; i++) {
+  sum += i;
+  if (sum >= 100) {
+    console.log("Total sum:", sum);
+    console.log("Loop is terminated as sum reaches/exceeds 100");
+    break;
+  }
+}
+
+
+//  Task-3:
+
+
+for (let i = 1; i <= 100; i++) {
+  console.log(i);
+  if (i % 4 === 0 || i % 9 === 0 || i % 16 === 0) {
+    console.log("First number divisible by 4, 9, or 16 found:", i);
+    break;
+  }
+}
+
+// part 2 javascript
+// Task 1
+for (let i = 1; i <= 40; i++) {
+  if (i % 2 === 0) {
+    console.log(i);
+  }
+}
+
+// Task 2
+for (let i = 55; i <= 85; i++) {
+  if (i % 5 === 0) {
+    console.log(i);
+  }
+}
+
+
+// javascript
+// Task 1
+console.log('Task-1');
+console.log('"I will dedicate at least 6 hours daily for the next 60 days!" Print this message 60 times.');
+for (let i = 1; i <= 60; i++) {
+    console.log(`Message ${i}: "I will dedicate at least 6 hours daily for the next 60 days!"`);
+}
+
+// Task 2
+console.log('Task-2');
+console.log('Find all odd numbers from 61 to 100:');
+for (let i = 61; i <= 100; i += 2) {
+    console.log(i);
+}
+console.log('Find all even numbers from 78 to 98:');
+for (let i = 78; i <= 98; i += 2) {
+    console.log(i);
+}
+
+// Task 3
+console.log('Task-3');
+console.log(' numbers from 91 to 129:');
+let oddSum1 = 0;
+for (let i = 91; i <= 129; i += 2) {
+    oddSum1 += i;
+}
+console.log('Sum:', oddSum1);
+
+console.log('numbers  51 to 85:');
+let evenSum1 = 0;
+for (let i = 52; i <= 85; i += 2) {
+    evenSum1 += i;
+}
+console.log('Sum:', evenSum1);
+
+// Task 4
+console.log('Task-4');
+console.log(' number 9:');
+for (let i = 1; i <= 10; i++) {
+    console.log(`9 x ${i} = ${9 * i}`);
+}
+
+
+function countdown(start, end) {
+  for (let i = start; i >= end; i--) {
+      console.log(i);
+  }
+}
+
+countdown(81, 65);
+
+// This code will print the numbers from 81 to 65 in the c
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+umbers = [23, 22, 43, 54, 65, 46, 34, 90,]
+
+// numbers = [4] =
+
+
+
+console.log(numbers[4]);
+
+
+
+
+
